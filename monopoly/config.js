94a8f1e02ddb8ac4ebf747280b3bb28cd13f006b
Mehdi Allaoui <mehdialaoui123456@gmail.com>
2026-05-28 20:58:42 +0100

    Add cannons fight game

---
+++ monopoly/config.js
@@ -0,0 +1,9 @@
+const _fbConfig = {
+  apiKey:            "AIzaSyD3w3nzf89A7HYjtCsNkQjvVaCJqes0Jzs",
+  authDomain:        "ingames-9c93b.firebaseapp.com",
+  databaseURL:       "https://ingames-9c93b-default-rtdb.firebaseio.com",
+  projectId:         "ingames-9c93b",
+  storageBucket:     "ingames-9c93b.firebasestorage.app",
+  messagingSenderId: "711825788462",
+  appId:             "1:711825788462:web:bf1e4a59d4d18a543f59bf"
+};
